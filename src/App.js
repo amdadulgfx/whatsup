dda03ccea9c4b0cfc8a25f32ada92111db3cf7c2
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-11-21 10:16:51 +0600

    register page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Login/Login/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Register/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Home></Home>} />
           <Route path='/home' element={<Home></Home>} />
           <Route path='/login' element={<Login></Login>} />
+          <Route path='/register' element={<Register></Register>} />
 
         </Routes>
       </Router>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../../images/icons8-message-64.png'
-const Login = () => {
+import logo from '../../images/icons8-message-64.png'
+const Register = () => {
     return (
         <div className='py-4 space-y-6 h-screen flex  flex-col items-center  justify-center bg-gray-100'>
             <div className='space-y-6'>
@@ -9,7 +9,7 @@ const Login = () => {
 
                     <img className='mx-auto p-3' src={logo} alt="whatsup logo" />
                 </div>
-                <h2 className='text-4xl font-medium'>Login</h2>
+                <h2 className='text-4xl font-medium'>Register</h2>
             </div>
             <div className='p-5 filter drop-shadow bg-white rounded-lg w-80'>
                 <form action="" className='space-y-3 mx-auto w-full'>
@@ -17,15 +17,15 @@ const Login = () => {
                     <input className='px-3 py-2 bg-gray-200 rounded-md w-full' type="email" placeholder='Email' /> <br />
                     <input className='px-3 py-2 bg-gray-200 rounded-md w-full' type="password" placeholder='Password' /> <br />
 
-                    <button className='bg-purple-500 text-white w-full px-3 py-2 rounded-md' type="submit">Login</button>
+                    <button className='bg-purple-500 text-white w-full px-3 py-2 rounded-md' type="submit">Register</button>
 
                     <hr />
                 </form>
-                <Link to='/register'><button className='bg-green-500 text-white w-full px-3 py-2 rounded-md'>Create New Account</button></Link>
+                <Link to='/login'><button className='bg-green-500 text-white w-full px-3 py-2 rounded-md'>Login</button></Link>
 
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;
