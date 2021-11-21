22ede77df484a6c6de28ab893f3cff4bdd935db5
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-11-21 10:07:57 +0600

    login route and design added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 // import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Login/Login/Home/Home';
+import Login from './Pages/Login/Login/Login';
 
 function App() {
   return (
     <div className="App ">
-      <Home></Home>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home></Home>} />
+          <Route path='/home' element={<Home></Home>} />
+          <Route path='/login' element={<Login></Login>} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -10,13 +10,14 @@ const Login = () => {
                 </div>
                 <h2 className='text-4xl font-medium'>Login</h2>
             </div>
-            <div className='p-5 filter drop-shadow bg-white rounded-lg '>
-                <form action="" className='space-y-3'>
+            <div className='p-5 filter drop-shadow bg-white rounded-lg w-80'>
+                <form action="" className='space-y-3 mx-auto w-full'>
 
-                    <input className='px-3 py-2 bg-gray-200 rounded-md' type="email" placeholder='Email' /> <br />
-                    <input className='px-3 py-2 bg-gray-200 rounded-md' type="password" placeholder='Password' /> <br />
+                    <input className='px-3 py-2 bg-gray-200 rounded-md w-full' type="email" placeholder='Email' /> <br />
+                    <input className='px-3 py-2 bg-gray-200 rounded-md w-full' type="password" placeholder='Password' /> <br />
 
                     <button className='bg-purple-500 text-white w-full px-3 py-2 rounded-md' type="submit">Login</button>
+
                 </form>
             </div>
         </div>
